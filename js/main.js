71acc8147132c0524d756a0cfda61a81b3b47521
ghorbeldev <adem.ghorbel9@gmail.com>
2021-08-06 22:57:40 +0100

    leon

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-const links = document.querySelector(".links");
-const scrollToTop = document.querySelector(".scroll-to-top");
-console.log(scrollToTop);
-links.addEventListener("click", () => {
-  links.classList.toggle("active");
-});
-scrollToTop.addEventListener("click", e => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelector(".links");
+  const scrollToTop = document.querySelector(".scroll-to-top");
+  const features = document.querySelectorAll(".feature");
+  features.forEach((feature, indx) => {
+    feature.dataset.aos = "fade-up";
+    feature.dataset.aosDelay = 100 * indx;
   });
+
+  links.addEventListener("click", () => {
+    links.classList.toggle("active");
+  });
+  scrollToTop.addEventListener("click", e => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+  AOS.init({duration: 800});
 });
