439d3270c2b9d117e14ddfaae8cf28124d0ae523
ghorbeldev <adem.ghorbel9@gmail.com>
2022-01-19 12:17:17 +0100

    add README file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Leon Portfolio Template
+
+## Technologies used
+
+- HTML5
+- CSS3
+- JavaScript
+
+### Template Screenshot
+
+[TEMPLATE DEMO](https://ghorbeldev.github.io/leon-template/)
+
+![WEBSITE PREVIEW](./leon-template.png)
--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelector(".links");
-  const scrollToTop = document.querySelector(".scroll-to-top");
-  const features = document.querySelectorAll(".feature");
-  features.forEach((feature, indx) => {
-    feature.dataset.aos = "fade-up";
-    feature.dataset.aosDelay = 100 * indx;
-  });
-  const services = document.querySelectorAll(".service");
-  services.forEach((service, indx) => {
-    service.dataset.aos = "fade-up";
-    service.dataset.aosDelay = 100 * indx;
-  });
-  const cards = document.querySelectorAll(".card");
-  cards.forEach((card, indx) => {
-    card.dataset.aos = "fade-up";
-    card.dataset.aosDelay = 100 * indx;
-  });
+document.addEventListener('DOMContentLoaded', () => {
+	const links = document.querySelector('.links');
+	const scrollToTop = document.querySelector('.scroll-to-top');
+	const features = document.querySelectorAll('.feature');
+	features.forEach((feature, indx) => {
+		feature.dataset.aos = 'fade-up';
+		feature.dataset.aosDelay = 100 * indx;
+	});
+	const services = document.querySelectorAll('.service');
+	services.forEach((service, indx) => {
+		service.dataset.aos = 'fade-up';
+		service.dataset.aosDelay = 100 * indx;
+	});
+	const cards = document.querySelectorAll('.card');
+	cards.forEach((card, indx) => {
+		card.dataset.aos = 'fade-up';
+		card.dataset.aosDelay = 100 * indx;
+	});
 
-  links.addEventListener("click", () => {
-    links.classList.toggle("active");
-  });
-  scrollToTop.addEventListener("click", e => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  });
-  AOS.init({duration: 800});
+	links.addEventListener('click', () => {
+		links.classList.toggle('active');
+	});
+	scrollToTop.addEventListener('click', e => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	});
+	AOS.init({ duration: 800 });
 });
